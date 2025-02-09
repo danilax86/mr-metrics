a911f3eb91d26aa8844cc18263d3aa4103e8bb43
Danila Gorelko <hello@danilax86.space>
2025-02-09 11:25:18 +0300

    Fix typo

--- internal/db/migrations/0002_add_timestamps_for_mrs.sql
+++ internal/db/migrations/0002_add_timestamps_for_mrs.sql
@@ -10,7 +10,7 @@ ALTER TABLE merged_mrs
     DROP CONSTRAINT merged_mrs_pkey,
     ADD PRIMARY KEY (id);
 
-CREATE INDEX idx_merg“ed_mrs_user_project ON merged_mrs (username, project_id);
+CREATE INDEX idx_merged_mrs_user_project ON merged_mrs (username, project_id);
 CREATE INDEX idx_merged_mrs_created_at ON merged_mrs (created_at);
 
 COMMIT;
